31c516d07cd664981be16af796769ca50d8e7594
kreuzerkrieg <kreuzerkrieg@gmail.com>
2020-02-21 22:48:30 +0200

    TurboBase64

--- ports/turbobase64/portfile.cmake
+++ ports/turbobase64/portfile.cmake
@@ -1,34 +1,21 @@
-include(vcpkg_common_functions)
-
-if (NOT VCPKG_CMAKE_SYSTEM_NAME)
-    set(EXEC_ENV "Windows")
-else ()
-    set(EXEC_ENV "${VCPKG_CMAKE_SYSTEM_NAME}")
-endif ()
-
-if (NOT EXEC_ENV STREQUAL "Linux")
-    message(FATAL_ERROR "ProjectName Super Duper Library currently only supports Linux platforms")
-    message(STATUS "Please consider creating a port for Windows")
-endif ()
+vcpkg_fail_port_install(ON_TARGET "UWP" "Windows")
 
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO powturbo/Turbo-Base64
-        REF master
-        SHA512 066d63be9b6cbe08e21aa7e59a721abd671e67937ed7af3a2ec8eafb6606173097c6364d239a5b708159bbfed758757ffebb69035384fa623963bcf0664c7d6c
+        REF 5257626d2be17a3eb23f79be17fe55ebba394ad2
+        SHA512 7843652793d20c007178cd2069f376578d39566f6e558d7a2ea4f453046ebf5729e7208d6aca205fcca4d2174a3c4de3a6bc841d455778ebf95b3bdaad08c399
         HEAD_REF master
 )
 
+configure_file(${CURRENT_PORT_DIR}/CMakeLists.txt ${SOURCE_PATH}/CMakeLists.txt COPYONLY)
+
 vcpkg_configure_cmake(
-        SOURCE_PATH ${CMAKE_CURRENT_LIST_DIR}
+        SOURCE_PATH ${SOURCE_PATH}
         PREFER_NINJA
-        OPTIONS
-        -DSOURCE_PATH=${SOURCE_PATH}
 )
 
 vcpkg_install_cmake()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include ${CURRENT_PACKAGES_DIR}/debug/share)
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
-
-vcpkg_test_cmake(PACKAGE_NAME ${PORT})
